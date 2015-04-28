c4c5fc5e444e3b3f26ff021e92f6736adb32b821
D3v1an <d3v1an.tux@gmail.com>
2015-04-28 12:40:19 -0500

    Done

--- app/routes.php
+++ app/routes.php
@@ -65,6 +65,16 @@ Route::get('/test/{actor}:{data_init}:{data_end}', function($actor,$data_init,$d
 
         }
 
+        $_out = null;
+
+        foreach ($data as $d) {
+            if(!isset($d['fecha'])) {
+                $_out = $d;
+            }
+        }
+
+        return $_out;
+
         $file_name = 'Reporte Sonora ' . date('Y-m-d.H-i-s');
 
         Excel::create($file_name, function($excel) use($data) {
