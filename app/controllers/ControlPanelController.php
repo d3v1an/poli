d49f587ebf85f4a5f26c38d6d24b5c84c06c9c52
D3v1an <d3v1an.tux@gmail.com>
2015-04-28 15:21:45 -0500

    Done

--- app/controllers/ControlPanelController.php
+++ app/controllers/ControlPanelController.php
@@ -1890,7 +1890,7 @@ class ControlPanelController extends BaseController {
 
 		}
 
-		$file_name = 'Reporte Sonora ' . date('Y-m-d.H-i-s');
+		$file_name = 'Reporte Sonora - Completo - ' . $data_init . ' - ' $data_end;
 
 		Excel::create($file_name, function($excel) use($data) {
 
